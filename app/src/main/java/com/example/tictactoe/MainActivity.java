4a92a9a01f53cc604ba6ac3e293d24494f6afcaf
Aravind Naveen <aravindmyd@gmail.com>
2019-05-09 09:00:00 +0530

    Added some Logic to prevent clicking the same Place.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.tictactoe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0; // 1 for red and 0 for yellow
     int[] gameState = {2,2,2,2,2,2,2,2,2}; //Later we can update the states of the player. Intially 2 means unplayed.
+    int [][] winningCombinations ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{2,4,6},{0,4,8}}; // Total winnings position
+    boolean isPlayable = true;
 
 
     public void animation(View view){
@@ -19,10 +24,9 @@ public class MainActivity extends AppCompatActivity {
         //counter.setTranslationY(-2000f);
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
-        System.out.println(tappedCounter+" is clicked!");
 
 
-        if(gameState[tappedCounter] == 2) {
+        if(gameState[tappedCounter] == 2 &&  isPlayable) {
 
             // Checking if the button is already clicked and prevents clicking more than one time.
 
@@ -40,12 +44,80 @@ public class MainActivity extends AppCompatActivity {
                 activePlayer = 0;
             }
             counter.animate().translationYBy(2000f).rotation(360).setDuration(360);
+
+            // checking winning positions
+
+            for(int[] winningPosition: winningCombinations){
+
+                if(gameState[winningPosition[0]]== gameState[winningPosition[1]]&&
+                gameState[winningPosition[1]] == gameState[winningPosition[2]]&&
+                gameState[winningPosition[0]]!=2
+                ){
+                    isPlayable=false;
+
+                    String winnerMsg = "Red has won! ";
+
+                    if(gameState[winningPosition[0]] ==0) {
+                         winnerMsg = "Yellow has won! ";
+
+                    }
+
+                    TextView winnerTextView = findViewById(R.id.winnerTextView);
+                    winnerTextView.setText(winnerMsg);
+                    // Making the winner Layout visible
+
+                    LinearLayout playAgainLayout = findViewById(R.id.PlayAgainLayout);
+                    playAgainLayout.setVisibility(View.VISIBLE);
+
+                }
+                else {
+                    boolean isGameOver = true;
+
+                    for(int gameIndex:gameState){
+
+                        if(gameIndex == 2) {
+                            isGameOver = false;
+                            break;
+                        }
+                    }
+
+                    if(isGameOver){
+
+                        TextView winnerTextView = findViewById(R.id.winnerTextView);
+                        winnerTextView.setText("Its a Draw. Play one More!");
+
+                        LinearLayout playAgainLayout = findViewById(R.id.PlayAgainLayout);
+                        playAgainLayout.setVisibility(View.VISIBLE);
+                    }
+            }
+
+        }
+
+
         }
 
     }
 
+    public  void playAgain(View view){
+        isPlayable = true;
 
+        // Resetting Game State
+        for(int i =0;i<gameState.length;i++){
+            gameState[i] = 2;
 
+        }
+        // Hide the winner Layout
+        LinearLayout playAgainLayout = findViewById(R.id.PlayAgainLayout);
+        playAgainLayout.setVisibility(View.INVISIBLE);
+
+        //Resetting the red and yellow coins in the grid Layout
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+        for(int i =0; i<gridLayout.getChildCount();i++){
+
+            ((ImageView)gridLayout.getChildAt(i)).setImageResource(0);
+        }
+
+    }
 
 
     @Override
